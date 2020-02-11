af1616b7835a761e4e8d8c3c3737127b856fcb41
Vertigo <vertigomaill@gmail.com>
2020-02-11 11:51:06 +0300

    Elbooks

--- src/components/Elbooks/Elbooks.jsx
+++ src/components/Elbooks/Elbooks.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import './Elbooks.css';
 
 const Elbooks = () => {
-  return <div>Elbooks</div>;
+  return (
+    <div>
+      <div className="user-icon">123</div>
+      <div className="user-icon__user-menu">456</div>
+      <div className="user-icon__user-menu_active">789</div>
+    </div>
+  );
 };
 
 export default Elbooks;
